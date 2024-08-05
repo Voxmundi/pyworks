6764b0d8050a4efb41794b7a6aa5f7257294cc1c
dataman <yamural@gmail.com>
2024-08-05 13:11:05 +0300

    solid completed

--- solid.py
+++ solid.py
@@ -16,43 +16,75 @@ class Order:
         for i in range(len(self.prices)):
             total += self.quantities[i] * self.prices[i]
         return total
+    
 
+class Authorizer(ABC):
 
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+class SMSAuth(Authorizer):
+    authorized = False
+
+    def verify_code(self,code):
+        print(f"Veriying code {code}")
+        self.authorized = True
+    
+    def is_authorized(self) -> bool:
+        return self.authorized
+    
+class NotRobot(Authorizer):
+    authorized = False
+
+    def not_robot(self):
+        print("You'r not a Robot")
+        self.authorized = True
+    
+    def is_authorized(self) -> bool:
+        return self.authorized
 
 
 class PaymentProcessor(ABC):
-
     @abstractmethod
     def pay(self,order):
         pass
 
 class DebitPayment(PaymentProcessor):
 
-    def __init(self,security_code):
+    def __init__(self,security_code, authorizer:Authorizer):
         self.security_code = security_code
+        self.authorizer = authorizer
 
 
-    def pay(self,order,security_code):
+    def pay(self,order):
+        if not self.authorizer.is_authorized():
+            raise Exception ("Not Authorized")
+        
         print ("processing debit payment")
         print (f"Verifiying security code {self.security_code}")
         order.status = "paid"
 
 class CreditPayment(PaymentProcessor):
 
-    def __init(self,security_code):
+    def __init__(self,security_code):
         self.security_code = security_code
 
-    def pay(self,order,security_code):
+    def pay(self,order):
         print (f"processing credit payment {order.total_price()}")
-        print (f"Verifiying security code {self.ecurity_code}")
+        print (f"Verifiying security code {self.security_code}")
         order.status = "paid"  
 
 class PaypallPayment(PaymentProcessor):
 
-    def __init(self,email):
-        self.email = email
+    def __init__(self,security_code, authorizer:Authorizer):
+        self.security_code = security_code
+        self.authorizer = authorizer
 
-    def pay(self,order,security_code):
+    def pay(self,order):
+        if not self.authorizer.is_authorized():
+            raise Exception ("Not Authorized")
+        
         print (f"processing paypall payment {order.total_price()}")
         print (f"Verifiying security code {self.email}")
         order.status = "paid" 
@@ -63,12 +95,14 @@ order = Order()
 order.add_item("Keyboard", 1, 150)
 order.add_item("SSD", 2, 250)
 order.add_item("USB", 5, 50)
-
 print(order.total_price())
 
-payment = CreditPayment()
 
-payment.pay(order, "asdas")
+authorizer = NotRobot()
+payment = DebitPayment('xxx-eee-2',authorizer)
+authorizer.not_robot()
+print (payment.security_code)
+payment.pay(order)
 
 print(order.status)
 
